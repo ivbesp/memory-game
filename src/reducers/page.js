27b98e54cd9c94d98f7e54033da0a9e9a46eb32f
ivbesp <iv.besp@yandex.ru>
2018-03-28 15:55:22 +0300

    изменен под объект начального состояния

--- src/reducers/page.js
+++ src/reducers/page.js
@@ -3,13 +3,13 @@ import InitialState from './../initialstate';
 export const page = (state = InitialState.page, action) => {
     switch(action.type) {
         case 'RENDER_STARTPAGE':
-            return Object.assign({},state,'StartPage');
+            return Object.assign({},state,{current: 'StartPage'});
 
         case 'RENDER_GAMEPAGE':
-            return Object.assign({},state,'GamePage');
+            return Object.assign({},state,{current: 'GamePage'});
 
         case 'RENDER_ENDPAGE':
-            return Object.assign({},state,'EndPage');
+            return Object.assign({},state,{current: 'EndPage'});
 
         default:
             return state;
