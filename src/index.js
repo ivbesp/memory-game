ed016ebf4af346c306c2e54b1b085c0b33e55501
ivbesp <iv.besp@yandex.ru>
2018-03-28 14:15:55 +0300

    добавлен роутер для рендеринга страниц

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, combineReducers} from 'redux';
 import {Provider} from 'react-redux';
 import './scss/style.scss';
 import configureStore from './store/configureStore';
 import App from './containers/App';
-import Home from './containers/Home';
+import Router from './containers/Router';
 
 const store = configureStore();
 
 ReactDOM.render(
     <Provider store={store}>
         <App>
-            <Home/>
+            <Router/>
         </App>
     </Provider>,
     document.getElementById('root')
